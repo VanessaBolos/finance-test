a462242596c631dfd1912c861a241f36c5978039
Vanz <essa.olfie@gmail.com>
2024-12-22 04:34:11 -0800

    init commit

---
+++ app/layout.js
@@ -0,0 +1,25 @@
+"use client";
+
+import "./globals.css";
+
+import Nav from "@/components/Navigation";
+
+import FinanceContextProvider from "@/lib/store/finance-context";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <body>
+        <FinanceContextProvider>
+          <Nav />
+          {children}
+        </FinanceContextProvider>
+      </body>
+    </html>
+  );
+}
